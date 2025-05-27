ba2d877d69acb35fd7f33475e23f3e6470ed2231
imseohyeon <forestlight749@gmail.com>
2025-05-27 11:45:29 +0900

    feat (0527): 타입별칭

---
+++ a_kotlin/src/main/kotlin/e_oop/j_typealias/a_typealias.kt
@@ -0,0 +1,25 @@
+package e_oop.j_typealias
+
+// 타입에 별칭을 부여
+typealias UserId = String
+typealias ProductId = Long
+typealias Price = Int
+typealias Items = List<ProductId>
+
+data class Order(
+    val orderId: Long,
+    val userId: String,
+    val productId: Long,
+    val items: List<Long>,
+    val totalPrice: Int
+)
+
+fun main(){
+    val order = Order(
+        orderId = 12345L,
+        userId = "test",
+        productId = 2222L,
+        items = listOf(1,2,3),
+        totalPrice = 50000)
+    println(order)
+}
