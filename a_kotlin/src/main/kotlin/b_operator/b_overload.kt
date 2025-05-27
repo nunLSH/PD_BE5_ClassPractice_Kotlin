9a40dc7233c25d9c5aae4fbf1ffb948b90c7aebb
imseohyeon <forestlight749@gmail.com>
2025-05-27 12:08:43 +0900

    feat (0527): 연산자 overload

---
+++ a_kotlin/src/main/kotlin/b_operator/b_overload.kt
@@ -0,0 +1,17 @@
+package b_operator
+
+data class User(
+    val email:String,
+    val password:String
+){
+    operator fun get(key: String): Any?{
+        return User::class.members.find { it.name == key }?.call(this)
+    }
+}
+
+fun main(){
+    val user = User("lsh@grepp.com", "1234")
+    println(user["email"])
+    println(user["toString"])
+
+}
