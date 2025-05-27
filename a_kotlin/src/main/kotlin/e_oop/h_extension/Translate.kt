bbf820484d22b437f14a3b19af97477458854ba8
imseohyeon <forestlight749@gmail.com>
2025-05-27 10:53:52 +0900

    feat (0527): (추가) 확장 함수, infix 함수

--- a_kotlin/src/main/kotlin/e_oop/h_extension/Translate.kt
+++ a_kotlin/src/main/kotlin/e_oop/h_extension/Translate.kt
@@ -1,11 +1,51 @@
 package e_oop.h_extension
 
-// 확장함수의 확장속성
+private var languages = mutableListOf<Payment.() -> String>()
+
+private val translate: Payment.() -> List<String> = {
+    languages.map{it()}.toList()
+}
+
+// infix 함수
+// 중위표현식으로 호출할 수 있는 함수
+// 매개변수가 반드시 1개만 존재
+infix fun Payment.printTranslate(nation: Payment.()->String){
+    println(nation())
+}
+
+// 확장함수와 확장속성
 private val Payment.chinaPrice:String
     // payment 의 this 가 전달됨
     get() = "${this.payPrice / 200} wian"
 
+private val Payment.americaPrice:String
+    get() = "${this.payPrice / 1400} dollar"
+
+private val Payment.spainPrice:String
+    get() = "${this.payPrice / 1400} eur"
+
+// 확장함수 안에서 확장 속성 가져다 쓸 수 있음
+private val china:Payment.() -> String = {
+    "${this.coffee}[${this.orderCnt} 杯] $chinaPrice"
+}
+
+private val america:Payment.() -> String = {
+    "${this.coffee}[${this.orderCnt} cup] $americaPrice"
+}
+
+private val spain:Payment.() -> String = {
+    "${this.coffee}[${this.orderCnt} tazas] $spainPrice"
+}
+
 fun main(){
     val payment = Payment("아메리카노", 3, 9000)
-    println(payment.chinaPrice)
+//    println(payment.chinaPrice)
+//    println(payment.americaPrice)
+//    println(payment.spainPrice)
+//    println(payment.china())
+    languages.addAll(arrayOf(spain, china, spain))
+    println(payment.translate())
+    payment printTranslate china
+    payment printTranslate america
+    payment printTranslate spain
 }
